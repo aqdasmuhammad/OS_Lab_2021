f835349e4d6f9be6792920404065dbc97096a450
Muhammad Aqdas <56063389+aqdasmuhammad@users.noreply.github.com>
2021-06-26 20:46:19 +0500

    Update sch_pri.c

--- Lab3/sch_pri.c
+++ Lab3/sch_pri.c
@@ -12,177 +12,27 @@
 
 #include "cpu.h"
 
-struct node * head = NULL;
-struct node * current = NULL;
-struct node * new = NULL;
-struct node * final = NULL;
+struct node *list = NULL; //list for storing tasks
 
-void add(char * name, int priority, int burst) {
-	
-  if (head == NULL) {
-    head = malloc(sizeof(struct node));
-
-    // set the name of the task 
-    head -> task = malloc(sizeof(struct task));
-    head -> task -> name = name;
-    head -> task -> burst = burst;
-    head -> task -> priority = priority;
-    // set the next node to be null
-    head -> next = NULL;
-
-    current = head;
-  
-  } else {
-
-    new = malloc(sizeof(struct node));
-
-    new -> task = malloc(sizeof(struct task));
-    new -> task -> burst = burst;
-    new -> task -> name = name;
-    new -> task -> priority = priority;
-    // if current->next is NULL
-    if (!(current -> next)) {
-      if (((new -> task -> priority) < (current -> task -> priority)) || ((new -> task -> priority) ==(current -> task -> priority))) {
-        current -> next = new; // head points to second node 
-        new -> next = NULL;
-		
-      }
-	// if the second node burst is smaller than the current burst  
-	  else {
-		 
-	  // set new to point to head which is in the second position
-	  new->next = current; 	
-	  // head now holds the address of new which is in the first position
-	  head = new;
-	  // reset current to new
-      current = new;	  
-	  // we still have the second node connected to null  
-      }	   
-    }
- 
-	// T3 and on execute from here I think 
-		else {
-    	  
-	  while(1) {
-    
-	 	  
-      if ((new -> task -> priority > current -> next -> task -> priority) ) {
-	  
-	  if (new->task->priority < current->task->priority) {
-	  new->next = current->next;
-	  current->next = new ;
-	  current = head; 
-	  break;
-	  }
-	  else if (new->task->priority > current->task->priority) {
-
-	  head = new;
-      new->next = current;
-	  current = head;
-	  break;
-	  }
-	  // if the new priority == the current priority
-	  else if(new->task->priority == current->task->priority) {
-		new->next = current->next;
-        current->next = new;	
-        break;		
-		  
-	  }  
-	  
-	  }	  
-	   
-      
-	  
-	  else if (new -> task -> priority == current -> next -> task -> priority) {
-	   current = current -> next ;
-	   
-	   if (current->next == NULL) {
-	  
-	    
-       new->next = NULL;
-	   
-	   }
-	   
-	   
-	   else if (new-> task-> priority == current->next->task->priority) {
-		   
-		current = current -> next ;   
-		new->next = current->next;   
-		   
-	   }   
-	   
-	   else {
-		   
-	   new->next = current->next;	   
-		   
-	   }   
-	   
-	    current->next = new;
-	  	   
-	   current = head;		    
-		break;  
-	  }  
-	  
-	  
-	  
-	  // if the new priority is less than the current priority
-	  else if ((new -> task -> priority) <(current -> next -> task -> priority)) {
-	  current = current -> next;
-	  
-	  if ( current->next == NULL) {
-	//printf("testing"); 
-	  current->next = new;  
-	  new->next = NULL;
-	  current = head;
-	  break;
-
-	  
-	  }	  
-	  
-	   
-      }	  
-	   	  
-		  
-		  
-		  
-	  }
-
-
-
-	  
-    }
-  }
- 
+void add(char * name, int priority, int burst) { //function to copy arguments into structure and apending task in list
+	Task *task = malloc(sizeof(Task));
+	strcpy(task->name,name);
+	task->burst = burst;
+	task->priority = priority;
+	insert(&list,task); //appending task in list
 }
 
 // invoke the scheduler
 void schedule() {
-int num = 0;
-float ResponseTime = 0;
-float turnaroundtime = 0;
-float WaitTime = 0;
-int burst = 0;
-
-  struct node * ref = head;
-  while (ref != NULL) {
-    num = num + 1;
-    run(ref -> task, ref -> task -> burst);
-    burst = burst + ref->task->burst;
-	turnaroundtime = turnaroundtime + burst ; //5 (5+10+5)20  50      (5 + 5+10 + 5+10+15)
-	if (ref->next !=NULL) {
-	ResponseTime = ResponseTime + burst;
+	while(list){
+		Task* task_to_run = list->task;
+		struct node *present = list; //head node will be present task
+		while(present){ //iteratig through whole list
+			if(present->task->priority >= task_to_run->priority) //check for highest priority
+				task_to_run = present->task; // exchanging tasks
+			present = present->next; //moving to next node
+		}
+		run(task_to_run,task->burst); //running task with highest priority
+		delete(&list,task); //deleting that task form task list
 	}
-    ref = ref -> next;
-  }
-
-  
-
-WaitTime = turnaroundtime - burst;
-
-printf("The average turnaround time is : %f time units \n" , (float)turnaroundtime/num);    
-printf("The average ResponseTime is : %f time units \n" , (float)ResponseTime/num);
-printf("The average WaitTime is : %f time units\n " , (float)WaitTime/num);
-
-
-
 }
